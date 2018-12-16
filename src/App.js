e550cf5b86d7ec2b5f7b12153e004eba1894e416
Justin Richmond-Decker <richmonddecker@gmail.com>
2018-12-16 15:09:18 -0700

    Set up app with react-router.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Drawn Out Process</h1>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>Drawn Out Process</h1>
+    </header>
+  </div>
+);
 
 export default App;
