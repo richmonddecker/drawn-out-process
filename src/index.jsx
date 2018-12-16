faa66c10d9cd99cce0d2ec9532a38adf39254fae
Justin Richmond-Decker <richmonddecker@gmail.com>
2018-12-16 15:13:01 -0700

    Changed .js files to .jsx

--- src/index.jsx
+++ src/index.jsx
@@ -6,9 +6,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render((
- 	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 ), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
