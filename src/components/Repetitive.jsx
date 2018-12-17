ec933a867b74f6b7b3c9824564c4b189cdac2007
Justin Richmond-Decker <richmonddecker@gmail.com>
2018-12-16 19:30:47 -0700

    Videos now resize appropriately.

--- src/components/Repetitive.jsx
+++ src/components/Repetitive.jsx
@@ -2,16 +2,39 @@ import React from "react";
 import videos from "../scripts/videos";
 import "../styles/App.css";
 
-const Repetitive = (props) => {
-  const name = props.match.params.name;
-  console.log("NAME IS: ", name);
-  return (
-    <div class="fill-screen">
-      <video class="fit-square" autoPlay loop muted>
+class Repetitive extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateDimensions = this.updateDimensions.bind(this);
+  }
+
+  render() {
+    const name = this.props.match.params.name;
+    return (
+      <div class="fill-screen">
+        <video height={this.state.height} width={this.state.width} autoPlay loop muted>
           <source src={videos[name]} type="video/mp4"/>
-      </video>
-    </div>
-  );
+        </video>
+      </div>
+    );
+  }
+
+  updateDimensions() {
+    const dimension = Math.min(window.innerWidth, window.innerHeight);
+    this.setState({width: dimension, height: dimension});
+  }
+
+  componentWillMount() {
+    this.updateDimensions();
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  componentWillUnMount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
 }
 
 export default Repetitive;
