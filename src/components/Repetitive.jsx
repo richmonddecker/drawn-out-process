dc399535f82f47669a057f4a9c2be9be3755e71d
Justin Richmond-Decker <richmonddecker@gmail.com>
2018-12-28 18:01:55 -0700

    Got 'save' and 'reset' working for the Repetitive elements.

--- src/components/Repetitive.jsx
+++ src/components/Repetitive.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import FileSaver from "file-saver";
 
 import { resetTrigger } from "../actions/trigger";
 
@@ -8,20 +9,39 @@ class Repetitive extends React.Component {
   constructor(props) {
     super(props);
     this.updateDimensions = this.updateDimensions.bind(this);
+    this.handleTriggers = this.handleTriggers.bind(this);
   }
 
   render() {
     return (
       <div className="fill-screen">
-        <video src={this.props.video} height={this.state.height} width={this.state.width} autoPlay loop muted>
+        <video id="repetitiveVideo" src={this.props.video} height={this.state.height} width={this.state.width} autoPlay loop muted>
         </video>
       </div>
     );
   }
 
+  handleTriggers(props) {
+    console.log("ALL PROPSL ", props)
+    if (props.save) {
+      props.clearSave();
+      // This will download/save the current video.
+      FileSaver.saveAs(props.video, props.tag);
+    }
+    if (props.reset) {
+      props.clearReset();
+      // This resets the video to the start.
+      document.getElementById("repetitiveVideo").currentTime = 0;
+    }
+  }
+
   updateDimensions() {
     const dimension = Math.min(window.innerWidth, window.innerHeight);
     this.setState({width: dimension, height: dimension});
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.handleTriggers(newProps);
   }
 
   componentWillMount() {
