2281ab60e7b1466f7d5d948a900c458646e3bef4
Justin Richmond-Decker <richmonddecker@gmail.com>
2019-01-11 18:48:57 -0700

    More additions to pointers app.

--- src/sketches/pointers.js
+++ src/sketches/pointers.js
@@ -6,15 +6,14 @@ const sketch = (p) => {
   let position = {};
   let velocity = {};
   let sizes = {};
-  let infoSpeed = 400;
+  let counts = {}; // how many pointers in x/y directions
+  let spacing = 0; // spacing between pointers
   p.isSquare = false;
   p.settings = {
-    string: "",
-    size: 100,
-    color: "#ff0000",
-    font: "Arial",
-    angle: 45,
-    speed: 200
+    thickness: 1,
+    length: 1,
+    count: 30,
+    speed: 400
   }
 
   class Pointer {
@@ -23,25 +22,26 @@ const sketch = (p) => {
       this.y = y;
       this.length = length;
       this.thickness = thickness;
-      this.angle = p.atan2(y - p.mouseY, x - p.mouseX);
+      this.angle = p.atan2(y - p.height / 2, x - p.width / 2);
       this.velocity = 0;
       this.colorFun = colorFun;
       this.trigger = Infinity;
     }
 
     armTrigger(x, y) {
-      this.setTriggerTime(p.millis() + 1000 * p.mag(x - this.x, y - this.y) / infoSpeed);
+      this.setTriggerTime(p.millis() + 1000 * p.mag(x - this.x, y - this.y) / p.settings.speed, x, y);
     }
 
-    setTriggerTime(time) {
+    setTriggerTime(time, x, y) {
       if (time < this.trigger) {
         this.trigger = time;
+        this.nextPoint = {x, y};
       }
     }
 
     checkTrigger() {
       if (p.millis() >= this.trigger) {
-        this.angle = this.newAngle;
+        this.pointTo(this.nextPoint.x, this.nextPoint.y);
         this.trigger = Infinity;
       }
     }
@@ -53,7 +53,7 @@ const sketch = (p) => {
     }
 
     pointTo(x, y) {
-      this.newAngle = p.atan2(y - this.y, x - this.x);
+      this.angle = p.atan2(y - this.y, x - this.x);
     }
 
     draw() {
@@ -65,53 +65,52 @@ const sketch = (p) => {
     }
   }
 
-  function getTextSize(string, size, font) {
-    p.textSize(size);
-    p.textFont(font);
-    return {height: size, width: p.textWidth(string)};
+  function getCounts() {
+    // Figure out how many pointers in the x and y directions.
+    if (p.width > p.height) {
+      counts.x = p.settings.count;
+      counts.y = p.floor(counts.x * p.height / p.width);
+      spacing = p.width / (counts.x - 1);
+    } else {
+      counts.y = p.settings.count;
+      counts.x = p.floor(counts.y * p.width / p.height);
+      spacing = p.height / (counts.y - 1);
+    }
+
   }
+
 
   function getCanvasSize() {
     const width = window.innerWidth;
     const height = window.innerHeight;
     if (p.isSquare) {
-      console.log("IS SAWUER)");
       return {width: p.min(width, height), height: p.min(width, height)}
     }
     return {width, height};
   }
 
-  function getSizes(string, size, font, canvas) {
-    // Determine the appropriate field size based on the text.
-    const text = getTextSize(string, size, font);
-    const field = {width: canvas.width - text.width, height: canvas.height - text.height * 0.8};
-    return {text, field};
-  }
-
-  function randomPlacement(width, height, speed, angle) {
-    position = {x: p.random(0, width), y: p.random(0, height)};
-    velocity = {x: speed * p.cos(p.PI/180 * angle), y: speed * p.sin(p.PI/180 * angle)};
-    const directionPicker = p.random(0, 4);
-    if (directionPicker < 1) {
-      velocity.x = -velocity.x;
-      velocity.y = -velocity.y;
-    } else if (directionPicker < 2) {
-      velocity.x = -velocity.x;
-    } else if (directionPicker < 3) {
-      velocity.y = -velocity.y;
-    }
-  }
-
   function makePointers() {
-    const count = 16;
-    const thick = 4;
+    const count = p.settings.count;
     const countX = p.floor(count * p.width / p.height);
     pointers = [];
     for (let i = 0; i < countX; i++) {
       for (let j = 0; j < count; j++) {
-        pointers.push(new Pointer(p.width * i / countX, p.height * j / count, p.height / count - thick, thick));
+        pointers.push(new Pointer(
+          p.width * i / countX,
+          p.height * j / count,
+          (p.height / count - p.settings.thickness) * p.settings.length,
+          p.settings.thickness
+        ));
       }
     }
+  }
+
+  p.mousePressed = function() {
+    const randomPoint = {x: p.random(0, p.width), y: p.random(0, p.height)};
+    pointers.forEach((x) => {
+      x.armTrigger(randomPoint.x, randomPoint.y)
+      x.pointTo(randomPoint.x, randomPoint.y)
+    });
   }
 
   p.mouseMoved = function() {
@@ -119,7 +118,9 @@ const sketch = (p) => {
     if (p.mouseX > p.width || p.mouseY > p.height || p.mouseX < 0 || p.mouseY < 0) {
       return;
     }
-    pointers.forEach((x) => {x.armTrigger(p.mouseX, p.mouseY); x.pointTo(p.mouseX, p.mouseY)});
+    pointers.forEach((x) => {
+      x.armTrigger(p.mouseX, p.mouseY);
+    });
   }
 
   p.touchMoved = function() {
